6de917fa1b6ade83de58ff0001ded26ace34be11
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-08 08:39:21 +0000

    Deploy MorMundHS-MA/GDV to github.com/MorMundHS-MA/GDV.git:gh-pages

--- src/LineChart.ts
+++ src/LineChart.ts
@@ -106,7 +106,7 @@ export class LineChart {
         this.country = country;
         this.data = [];
         for (const year of country.stats) {
-            if (year[1].gdp !== undefined && year[1].inequality.combined !== undefined) {
+            if (Number.isFinite(year[1].gdp) && Number.isFinite(year[1].inequality.combined)) {
                 this.data.push({year: year[0], stats: year[1]});
             }
         }
